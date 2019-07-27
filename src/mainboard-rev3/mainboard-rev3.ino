c803f71f5ec29420aa00bdcf49c59d9eec4f8396
Frank Migge <fm@frank4dd.com>
2019-07-27 20:15:05 +0900

    Add demo function to code

--- src/mainboard-rev3/mainboard-rev3.ino
+++ src/mainboard-rev3/mainboard-rev3.ino
@@ -21,6 +21,12 @@
 //#define DEBUG
 
 /* ------------------------------------------------- */
+/* SLOW and FAST set motor speed if stepper is used  */
+/* ------------------------------------------------- */
+#define SLOW 5000
+#define FAST 500
+
+/* ------------------------------------------------- */
 /* SD card - Arduino MKRZero onboard card reader     */
 /* ------------------------------------------------- */
 Sd2Card card;
@@ -94,10 +100,13 @@ const uint8_t ms21  = 7;     // GPIO pin stepper motor-1 step size 2
 const uint8_t ena1  = 8;     // GPIO pin stepper motor-1 enable
 const double mdecl = -7.583; // local magnetic declination
 uint8_t dippos1 = 0;         // dipswitch 1 position (extra tests)
-uint8_t dippos2 = 0;         // dipswitch 2 position (motor on)
+uint8_t dippos2 = 0;         // dipswitch 2 position (motor enable)
 uint16_t m1cpos = 0;         // stepper motor-1 current step position (0..1599)
 uint16_t m1tpos = 0;         // stepper motor-1 target step position (0..1599)
 boolean m1move = false;      // stepper motor-1 "move in progress" flag
+uint8_t opmode = 0;          // operations mode: 0 = normal, 1 = demo
+uint16_t day_azi[1440];      // daily azimuth angle 0..360, 1min interval, 2.88KB
+int16_t day_ele[1440];       // daily elevation angle -90 ..90, 1min interval 2.88KB
 
 /* ------------------------------------------------- */
 /* 'arduino logo', 64x32px, XBM format               */
@@ -212,6 +221,12 @@ void setup() {
   pinMode(push1, INPUT);
   pinMode(push2, INPUT);
   /* ------------------------------------------------- */
+  /* Pushbutton 1 (-) at startup triggers demo LOW=ON  */
+  /* demo mode switches main loop from min to sec exec */
+  /* ------------------------------------------------- */
+  if(digitalRead(push1) == LOW)
+  if(digitalRead(push1) == LOW) opmode = 1; // demo mode
+  /* ------------------------------------------------- */
   /* Set motor-1 control pins as output                */
   /* ------------------------------------------------- */
   pinMode(step1,OUTPUT); 
@@ -219,7 +234,6 @@ void setup() {
   pinMode(ena1,OUTPUT);
   pinMode(ms11,OUTPUT);
   pinMode(ms21,OUTPUT);
-  
   digitalWrite(ena1,HIGH); // Disable stepstick1
   /* ------------------------------------------------- */
   /* Enable I2C bus                                    */
@@ -424,7 +438,12 @@ void setup() {
   oled1.drawStr(0, 30, "4xMCP23017 OK");
   oled1.sendBuffer();
   delay(500);
-  
+
+  /* ------------------------------------------------- */
+  /* LED ring testing                                  */
+  /* ------------------------------------------------- */
+  oled1.drawStr(0, 47, "Test 32x LED");
+  oled1.sendBuffer();
   if(dippos1 == LOW) {
     led_lightcheck();
     /* ----------------------------------------------- */
@@ -434,33 +453,219 @@ void setup() {
     stepled_green(oled1, 20);
     stepled_red(oled2, 20);
     stepled_green(oled1, 20);
-  }  
+  }
+  oled1.drawStr(0, 63, "Lightshow ON");
+  oled1.sendBuffer();  
   lightshow(60);
   /* ------------------------------------------------- */
-  /* Read the solar position from file                 */
+  /* If demo mode was requested, display note on OLED2 */
   /* ------------------------------------------------- */
-  snprintf(binfile, sizeof(binfile), "20%d%02d%02d.BIN", 
-             rtc.year(),rtc.month(),rtc.day());
-  solar_position(binfile);
+  if(opmode == 1) {
+    snprintf(lineStr, sizeof(lineStr), "              ");
+    oled2.drawStr(0, 47, lineStr);
+    oled2.setFont(u8g2_font_inr19_mr);
+    snprintf(lineStr, sizeof(lineStr), "DEMO ON");
+    oled2.drawStr(0, 63, lineStr);
+    oled2.sendBuffer();
+  }
+  /* ------------------------------------------------- */
+  /* 2nd dip switch enables the motor function, LOW=ON */
+  /* ------------------------------------------------- */
+  if(dippos2 == LOW) {
+    /* ----------------------------------------------- */
+    /* Motor1 testing                                  */
+    /* ----------------------------------------------- */
+    oled1.drawStr(0, 47, "Test Stepper-1");
+    oled1.drawStr(0, 63, "              ");
+    oled1.sendBuffer();
+    motor1turn(2); /* test Azimuth motor1 for 2x times */
+    oled1.drawStr(0, 63, "Complete");
+    oled1.sendBuffer();
+    delay(1000);
+    m1cpos = 0;    /* Set motor1 current position = D1 */
+    m1cpos = 0;    /* Set motor1 target position = D1  */
+  }
   /* ------------------------------------------------- */
   /* Read sunrise sunset time from file                */
   /* ------------------------------------------------- */
-  snprintf(srsfile, sizeof(srsfile), "SRS-20%d.BIN", rtc.year());
+  snprintf(srsfile, sizeof(srsfile), "SRS-20%02d.BIN", rtc.year());
   get_suntime(srsfile);
   snprintf(riseStr, sizeof(riseStr), "rise %02d:%02d %03d",
            risehour,risemin, riseaz);
   snprintf(setStr, sizeof(setStr), "set  %02d:%02d %03d",
            sethour, setmin, setaz);
-  delay(1000);
   /* ------------------------------------------------- */
-  /* 2nd dip switch enables the motor function, LOW=ON */
+  /* Set daily data file name per RTC current date     */
   /* ------------------------------------------------- */
-  if(dippos2 == LOW) {
-    motor1turn(2); /* test Azimuth motor1 for 2x times */
-    delay(1000);
-    m1cpos = 0;    /* Set motor1 current position = D1 */
-    m1cpos = 0;    /* Set motor1 target position = D1  */
+  snprintf(binfile, sizeof(binfile), "20%02d%02d%02d.BIN", 
+             rtc.year(),rtc.month(),rtc.day());
+  /* ------------------------------------------------- */
+  /* Check for demo mode, output demo to OLED1 display */
+  /* ------------------------------------------------- */
+  if(opmode == 1) {
+    uint16_t i = 0;
+    uint8_t hr = 0;
+    uint8_t mi = 0;
+    fill_dayarray(binfile);
+    oled1.clearBuffer();
+    while(i < 1440) {       
+      /* ------------------------------------------------- */
+      /* 1st line show date and 'D'/'N' daytime/night flag */
+      /* ------------------------------------------------- */
+      snprintf(lineStr, sizeof(lineStr), "20%02d/%02d/%02d: ", 
+               rtc.year(),rtc.month(),rtc.day());
+      oled1.drawStr(0, 16, lineStr);
+      if(day_ele[i] >= 0) { daylight = true; oled1.drawStr(117, 16, "D"); }
+      else { daylight = false; oled1.drawStr(117, 16, "N"); }
+      /* ------------------------------------------------- */
+      /* 2nd line calculate and show time                  */
+      /* ------------------------------------------------- */
+      mi++;
+      if(mi == 60) { hr++; mi = 0; }
+      if(hr == 24) break;
+      snprintf(lineStr, sizeof(lineStr), "Time:    %02d:%02d", hr, mi);
+      oled1.drawStr(0, 30, lineStr);
+      /* ------------------------------------------------- */
+      /* 3rd line show azimuth angle                       */
+      /* ------------------------------------------------- */
+      snprintf(lineStr, sizeof(lineStr), "Azimuth:   %03d", day_azi[i]);
+      oled1.drawStr(0, 47, lineStr);
+      /* ------------------------------------------------- */
+      /* 4th line show elevation angle                     */
+      /* ------------------------------------------------- */
+      snprintf(lineStr, sizeof(lineStr), "Elevation: %03d", day_ele[i]);
+      oled1.drawStr(0, 63, lineStr);
+      oled1.sendBuffer();
+      /* ------------------------------------------------- */
+      /* Aquire compass sensor data                        */
+      /* ------------------------------------------------- */
+      compass.read();
+      heading = compass.heading() + mdecl;
+      /* ------------------------------------------------- */
+      /* print North heading to OLED2                      */
+      /* ------------------------------------------------- */
+      //snprintf(dirStr, sizeof(dirStr), "H%03d", (int)round((heading) * 10 / 10.0));
+      //oled2.setFont(u8g2_font_inr19_mr);
+      //oled2.drawStr(0,26, dirStr);
+      /* ------------------------------------------------- */
+      /* Convert heading angle to LED range value (0..32)  */   
+      /* ------------------------------------------------- */
+      hled = (uint8_t)round((heading / 11.25) * 10 / 10.0);
+      aled = (uint8_t)round((day_azi[i] / 11.25) * 10 / 10.0);
+      rled = (uint8_t)round((riseaz / 11.25) * 10 / 10.0);
+      sled = (uint8_t)round((setaz / 11.25) * 10 / 10.0);
+      /* ------------------------------------------------- */
+      /* If 32 is returned, it should roll over to 0       */   
+      /* ------------------------------------------------- */
+      if(hled > 31) hled = 0;
+      if(aled > 31) aled = 0;
+      if(rled > 31) rled = 0;
+      if(sled > 31) sled = 0;
+      /* ------------------------------------------------- */
+      /* Because LED-1 is 90 degrees offset, we compensate */
+      /* ------------------------------------------------- */
+      hled = hled + 24; if(hled > 31) hled = hled - 32;
+      /* ------------------------------------------------- */
+      /* Reverse counting to keep the LED aligned to North */
+      /* ------------------------------------------------- */
+      hled = 32 - hled; if(hled > 31) hled = 0;
+      /* ------------------------------------------------- */
+      /* Remaining LED follow heading                      */
+      /* ------------------------------------------------- */
+      aled = hled + aled; if(aled > 31) aled = aled - 32;
+      rled = hled + rled; if(rled > 31) rled = rled - 32;
+      sled = hled + sled; if(sled > 31) sled = sled - 32;
+      /* ------------------------------------------------- */
+      /* Set heading LED                                   */
+      /* ------------------------------------------------- */
+      if (hled != oldhled) {
+        if(oldhled < 16) mcp1.digitalWrite(oldhled, LOW);
+        else if(oldhled < 32) mcp2.digitalWrite((oldhled-16), LOW);
+        if(hled < 16) mcp1.digitalWrite(hled, HIGH);
+        if(hled > 15) mcp2.digitalWrite((hled-16), HIGH);
+        oldhled = hled;
+      }
+      /* ------------------------------------------------- */
+      /* Set azimuth LED                                   */
+      /* ------------------------------------------------- */
+      if (aled != oldaled) {
+        if(oldaled < 16) mcp3.digitalWrite(oldaled, LOW);
+        else if(oldaled < 32) mcp4.digitalWrite((oldaled-16), LOW);
+        if(aled < 16) mcp3.digitalWrite(aled, HIGH);
+        if(aled > 15) mcp4.digitalWrite((aled-16), HIGH);
+        oldaled = aled;
+      }
+      /* ------------------------------------------------- */
+      /* Set sunrise LED                                   */
+      /* ------------------------------------------------- */
+      if (rled != oldrled) {
+        if(oldrled < 16) mcp1.digitalWrite(oldrled, LOW);
+        else if(oldrled < 32) mcp2.digitalWrite((oldrled-16), LOW);
+        if(rled < 16) mcp1.digitalWrite(rled, HIGH);
+        if(rled > 15) mcp2.digitalWrite((rled-16), HIGH);
+        oldrled = rled;
+      }
+      /* ------------------------------------------------- */
+      /* Set sunset LED                                    */
+      /* ------------------------------------------------- */
+      if (sled != oldsled) {
+        if(oldsled < 16) mcp1.digitalWrite(oldsled, LOW);
+        else if(oldsled < 32) mcp2.digitalWrite((oldsled-16), LOW);
+        if(sled < 16) mcp1.digitalWrite(sled, HIGH);
+        if(sled > 15) mcp2.digitalWrite((sled-16), HIGH);
+        oldsled = sled;
+      }
+      /* ------------------------------------------------- */
+      /* motor1 target position follows azimuth LED value  */
+      /* if 2nd dip switch enabled the motor. DIP2: LOW=ON */
+      /* ------------------------------------------------- */
+      if(dippos2 == LOW) {
+        if(daylight) {
+          if(m1move == false) {
+            m1tpos = aled * 50;
+            if(m1tpos != m1cpos) {
+              motor1adjust(m1tpos, SLOW);
+            }
+          }
+        }
+        else {                /* nightfall, move motor-1 home */
+          if(m1move == false) {
+            if(m1tpos != 0) { /* are we already parked at D1? */
+              m1tpos = 0;     /* set D1 home position until sunrise */
+              motor1adjust(m1tpos, SLOW);
+            }
+          }
+        }
+      }
+      /* ------------------------------------------------- */
+      /* loop activities complete, increment loop counter  */     
+      /* Set the loop speed. 60000 = 60s = normal time     */
+      /* 100 = 1/10s intervals, 1440*0.1sec/60 = 2:24mins  */
+      /* ------------------------------------------------- */
+      delay(50);
+      i++;
+    }
+    /* ------------------------------------------------- */
+    /* After the DEMO turn off all LEDs and reset values */
+    /* ------------------------------------------------- */
+    mcp1.writeGPIOAB(0x00);
+    mcp2.writeGPIOAB(0x00);
+    mcp3.writeGPIOAB(0x00);
+    mcp4.writeGPIOAB(0x00);
+    hled = 0;             // heading LED (red)
+    oldhled = 33;         // 33 > max value 32, indicates "init"
+    aled = 0;             // azimuth LED (green)
+    oldaled = 33;         // 33 > max value 32, indicates "init"
+    rled = 0;             // sunrise LED (red)
+    oldrled = 33;         // 33 > max value 32, indicates "init"
+    sled = 0;             // sunset LED (red)
+    oldsled = 33;         // 33 > max value 32, indicates "init"
   }
+  delay(4000);
+  /* ------------------------------------------------- */
+  /* Intitialize azimuth/elevation position from file  */
+  /* ------------------------------------------------- */
+  solar_position(binfile);
   oled1.clearBuffer();
   oled2.clearBuffer();
 } // end setup
@@ -580,7 +785,7 @@ void loop() {
       if(m1move == false) {
         m1tpos = aled * 50;
         if(m1tpos != m1cpos) {
-          motor1adjust(m1tpos);
+          motor1adjust(m1tpos, SLOW);
         }
       }
     }
@@ -588,7 +793,7 @@ void loop() {
       if(m1move == false) {
         if(m1tpos != 0) { /* are we already parked at D1? */
           m1tpos = 0;     /* set D1 home position until sunrise */
-          motor1adjust(m1tpos);
+          motor1adjust(m1tpos, SLOW);
         }
       }
     }
@@ -599,8 +804,8 @@ void loop() {
   /* ------------------------------------------------- */
   if(dippos2 == LOW) {
     if(m1move == false) {
-      if(digitalRead(push1) == LOW) { motor1home(); }
-      if(digitalRead(push2) == LOW) { motor1led(); }
+      if(digitalRead(push1) == LOW) { motor1home(FAST); }
+      if(digitalRead(push2) == LOW) { motor1led(FAST); }
     }
   }
   /* ------------------------------------------------- */
@@ -1001,26 +1206,38 @@ void motor1turn(uint8_t count) {
 }
 
 /* ------------------------------------------------- */
-/* motor1adjust moves from the current position      */
-/* to the target position.                           */
+/* motor1adjust2 moves from the current position     */
+/* to the target position. position range 0..16000   */
 /* ------------------------------------------------- */
-void motor1adjust(uint16_t target) {
+void motor1adjust(uint16_t target, int speed) {
   uint16_t steps = 0;
   m1move = true;
   digitalWrite(ena1,LOW); /* Enable stepstick1 motor driver */
   if(m1cpos < m1tpos) {
-    digitalWrite(dir1,HIGH); /* clockwise motor direction   */
-    steps = m1tpos - m1cpos;
+    if(abs(m1cpos - m1tpos) < 800) {
+      digitalWrite(dir1,HIGH); /* run clockwise motor direction */
+      steps = m1tpos - m1cpos; /* get required steps to target  */
+    }
+    else {                    /* the other way around is shorter */
+      digitalWrite(dir1,LOW); /* move counterclockwise direction */
+      steps = 1600 - m1tpos + m1cpos;
+    }
   }
   else {
-    digitalWrite(dir1,LOW); /* counterclockwise  direction  */
-    steps = m1cpos - m1tpos;
+    if((m1cpos - m1tpos) < 800) {
+      digitalWrite(dir1,LOW); /* counterclockwise  direction  */
+      steps = m1cpos - m1tpos;
+    }
+    else {                     /* the other way around is shorter */
+      digitalWrite(dir1,HIGH); /* run clockwise motor direction   */
+      steps = 1600 - m1cpos + m1tpos;      
+    }
   }
   for(int x = 0; x < steps; x++) {
       digitalWrite(step1,HIGH);
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
       digitalWrite(step1,LOW); 
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
   }
   digitalWrite(ena1,HIGH); /* Disable stepstick1 motordriver */
   m1move = false;
@@ -1031,7 +1248,7 @@ void motor1adjust(uint16_t target) {
 /* motor1home moves from the current motor position  */
 /* back to starting point aligned with LED D1.       */
 /* ------------------------------------------------- */
-void motor1home() {
+void motor1home(int speed) {
   uint16_t steps = 0;
   m1move = true;
   digitalWrite(ena1,LOW); /* Enable stepstick1 motor driver */
@@ -1045,9 +1262,9 @@ void motor1home() {
   }
   for(int x = 0; x < steps; x++) {
       digitalWrite(step1,HIGH);
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
       digitalWrite(step1,LOW); 
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
   }
   digitalWrite(ena1,HIGH); /* Disable stepstick1 motordriver */
   m1move = false;
@@ -1055,19 +1272,20 @@ void motor1home() {
 }
 
 /* ------------------------------------------------- */
-/* motor1home moves from the current motor position  */
-/* back to starting point aligned with LED D1.       */
+/* motor1led moves from the current motor position   */
+/* one single LED back. The speed defines step time, */
+/* 500 = fast, 5000 = slow                           */
 /* ------------------------------------------------- */
-void motor1led() {
+void motor1led(int speed) {
   uint16_t steps = 50;
   m1move = true;
   digitalWrite(ena1,LOW); /* Enable stepstick1 motordriver */
   digitalWrite(dir1,HIGH); /* clockwise motor direction    */
   for(int x = 0; x < steps; x++) {
       digitalWrite(step1,HIGH);
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
       digitalWrite(step1,LOW); 
-      delayMicroseconds(500);
+      delayMicroseconds(speed);
   }
   digitalWrite(ena1,HIGH); /* Disable stepstick1 motordriver */
   m1move = false;
@@ -1110,3 +1328,25 @@ void swi2serial() {
   Serial.println(digitalRead(push2));
 }
 #endif
+
+/* ------------------------------------------------- */
+/* Read daily sun angles, store them in day arrays   */
+/* ------------------------------------------------- */
+void fill_dayarray(char *file) {
+  uint8_t linebuf[19];
+  uint16_t i = 0;
+  File bin = SD.open(file);
+  while (bin.available()) {
+    /* safety check. dayfile should have max 1440 data points */
+    if(i >= 1440) break;
+    bin.read(linebuf, sizeof(linebuf));
+    memcpy(&azimuth, &linebuf[3], sizeof(double));
+    memcpy(&zenith, &linebuf[11], sizeof(double));
+    // convert float to uint16_t (2 bytes) 0..360
+    day_azi[i] = (uint16_t) round(azimuth);
+    // convert float to int16_t (2 bytes) -90..90
+    day_ele[i] = 90 - (int16_t) round(zenith);
+    i++;
+  }
+  bin.close();
+}
